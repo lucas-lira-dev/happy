8bcc261f379555b407d2d3703409dba4ee2466c8
lucasrocha1999 <lucas.lira@cartaovaibem.com.br>
2020-10-14 22:12:33 -0300

    finalizing front-end resources

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { ThemesProvider } from './hooks/Themes';
-
 import Routes from './routes';
+
+import { ThemesProvider } from './hooks/Themes';
 
 const App: React.FC = () => {
 
